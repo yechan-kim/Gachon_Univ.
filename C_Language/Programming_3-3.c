d2e5dba9f84bdbfeaf9e79ff6e4b104fac5688d7
KimYeChan <tisckd@naver.com>
2020-09-21 10:48:13 +0900

    homework of 2020.09.18 class

--- C_Language/Programming_3-3.c
+++ C_Language/Programming_3-3.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+float power(float c, float d)
+{
+    float i, j;
+    j = c;
+
+    for (i = 0; i < d - 1; i++)
+        j *= c;
+
+    return j;
+}
+
 int main()
 {
     float x;
     printf("x의 값을 입력하시오: ");
     scanf("%f", &x);
-    printf("다항식의 값은 %.6f입니다.", (x + 76.587));
+    printf("다항식의 값은 %.5f입니다.", (3 * power(x, 3) - 9 * power(x, 2) + 9));
 
     return 0;
 }

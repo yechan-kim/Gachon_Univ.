b188e1f7675eda2f08fc1f087bc484d17a0c1775
KimYeChan <tisckd@naver.com>
2020-12-11 04:34:27 +0900

    self_study

--- C_Language/Programming_12-7.c
+++ C_Language/Programming_12-7.c
@@ -3,23 +3,21 @@
 
 int main()
 {
-    FILE *fp;
+    FILE* fp;
     char fname[100];
+    char str[100];
 
     printf("파일 이름을 입력하시오: ");
     scanf("%s", fname);
 
     fp = fopen(fname, "w");
 
-    while (1)
-    {
-        char str[100];
-        scanf("%s", str);
-        if (strlen(str) == 0)
-            break;
-        else
-            fputs(str, fp);
-    }
+    getchar();
+    do {
+        gets_s(str, sizeof(str));
+        fputs(str, fp);
+        fprintf(fp, "\n");
+    } while (strlen(str) != 0);
 
     fclose(fp);
 

38ff09eeccf2bdd8605defd939dbb6d667662fa5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-03-13 21:28:57 +0900

    Revert "2023.03110. class"
    This reverts commit 542b46f780888e69139e24dfd3e925187fa6fda6.

--- C++/ch02/cin_cout_EX/cin_cout_EX/main.cpp
+++ C++/ch02/cin_cout_EX/cin_cout_EX/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-	// 주소를 입력받아 출력하기
+	//주소를 입력받아 출력하기
 	char address[100];
+
 	cout << "주소입력: ";
-	// cin >> address;
-	// cout << "입력된 주소는 " << address << " 입니다." << endl;
-	cin.getline(address, 100, '\n');
+	cin >> address;
 	cout << "입력된 주소는 " << address << " 입니다." << endl;
+
+	return 0;
 }

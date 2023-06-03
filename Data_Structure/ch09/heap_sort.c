f6439c4d95f3f70b505ade96a026c4c8d34ecede
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-06-04 03:29:09 +0900

    self_study

--- Data_Structure/ch09/heap_sort.c
+++ Data_Structure/ch09/heap_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_ELEMENT 100
+#define MAX_ELEMENT 200
 
 typedef struct
 {
@@ -25,20 +25,24 @@ HeapType *create()
 // 초기화 함수
 void init(HeapType *h)
 {
-    h->size = 0;
+    h->heap_size = 0;
 }
-// 현재 요소의 개수가 size인 히프 h에 item을 삽입한다.
+
+// 현재 요소의 개수가 heap_size인 히프 h에 item을 삽입한다.
 // 삽입 함수
 void insert_max_heap(HeapType *h, int item)
 {
-    int i = ++(h->size);
-    while ((i != 1) && (item > h->heap[i / 2]))
+    int i;
+    i = ++(h->heap_size);
+    //  트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
+    while ((i != 1) && (item.key > h->heap[i / 2].key))
     {
         h->heap[i] = h->heap[i / 2];
         i /= 2;
     }
-    h->heap[i] = item;
+    h->heap[i] = item; // 새로운 노드를 삽입
 }
+
 // 삭제 함수
 int delete_max_heap(HeapType *h)
 {
@@ -61,40 +65,33 @@ int delete_max_heap(HeapType *h)
     return temp;
 }
 
-void print_heap(HeapType *h)
+void heap_sort(element a[], int n)
 {
-    for (int i = 1; i <= h->size; i++)
-    {
-        printf("%d: [%d], ", i, h->heap[i]);
-    }
-    printf("\n");
+    int i;
+    HeapType *h;
+    h = create();
+    init(h);
+
+    for (i = 0; i < n; i++)
+        insert_max_heap(h, a[i]);
+
+    for (i = (n - 1); i >= 0; i--)
+        a[i] = delete_max_heap(h);
+
+    free(h);
 }
+
+#define SIZE 8
 
 int main(void)
 {
-    int e[MAX_ELEMENT];
-    HeapType *heap;
+    int list[SIZE] = {23, 56, 11, 9, 56, 99, 27, 34};
 
-    heap = create(); // 히프 생성
-    init(heap);      // 초기화
+    heap_sort(list, SIZE);
 
-    // 삽입
-    for (int i = 1; i < MAX_ELEMENT; i++)
-    {
-        e[i]= rand()%99;
-        insert_max_heap(heap, e[i]);
-    }
-        
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", list[i].key);
+    printf("\n");
 
-    print_heap(heap);
-
-    // 삭제
-    for (int i = 1; i < MAX_ELEMENT; i++)
-    {
-        printf("< %d > \n", delete_max_heap(heap));
-    print_heap(heap);
-    }
-
-    free(heap);
     return 0;
 }

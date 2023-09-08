32a22f661f39c058e6cea35729a6d37f7f326f5b
yechan-kim <tisckd@naver.com>
2023-09-08 23:50:12 +0900

    2023.09.08_class

--- JAVA/test/src/main/java/HelloWorld.java
+++ JAVA/test/src/main/java/HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
+    String name;
+    String snum;
+
+    public HelloWorld(String name, String snum) { // 생성자
+        this.name = name;
+        this.snum = snum;
+    }
+
+    public void print() { // 메소드
+        System.out.printf("%s, %s\n",snum,name);
+    }
+
     public static void main(String[] args) {
-        System.out.println("202035130, kimyechan");
+        HelloWorld hello = new HelloWorld("김예찬", "202035130");
+        hello.print();
+        hello.name = "홍길동";
+        hello.snum = "202035000";
+        hello.print();
+        System.out.printf("%s, %s\n",hello.snum,hello.name);
     }
 }

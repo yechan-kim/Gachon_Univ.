8633326665945a1ea0ce980a1d63607f479169b4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-04-25 05:39:48 +0900

    self_study

--- Mobile_Programming/Week5_1/app/src/main/java/com/example/week5_1/MainActivity.java
+++ Mobile_Programming/Week5_1/app/src/main/java/com/example/week5_1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.week5_1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        setContentView(R.layout.activity_main);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        baseLayout.setBackgroundColor(Color.rgb(0, 255, 0));
+        setContentView(baseLayout, params);
+
+        Button btn = new Button(this);
+        btn.setText("버튼입니다.");
+        btn.setBackgroundColor(Color.MAGENTA);
+        baseLayout.addView(btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "코드로 생성한 버튼입니다.", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }

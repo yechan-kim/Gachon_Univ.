90a6b7f47dac40c1410858e5eaa4ed768ce0389c
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-15 06:30:30 +0900

    self_study

---
+++ Web_DB_Programming/basics/04/server-5.js
@@ -0,0 +1,23 @@
+// 라우팅 연습하기  (결과 비교 파일 : 04\results\server-5.js)
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const {method, url} = req;
+
+    res.setHeader("Content-Type", "text/plain");
+    if (method === 'GET' && url === '/home') {
+        res.statusCode = 200;
+        res.end("HOME");
+    } else if (method === 'GET' && url === '/about') {
+        res.statusCode = 200;
+        res.end("ABOUT");
+    } else {
+        res.statusCode = 404;
+        res.end("Not Found");
+    }
+});
+
+server.listen(3000, () => {
+  console.log("3000번 포트에서 서버 실행 중");
+});

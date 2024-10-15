b8c6be10599cf6d3d87c82af9466ce311e74c16e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-15 18:47:03 +0900

    self_study

--- Web_DB_Programming/myContacts/app.js
+++ Web_DB_Programming/myContacts/app.js
@@ -1,36 +1,23 @@
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
 const app = express();
+
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.status(200);
-    res.send("Hello Node!");
+    res.status(200).send("Hello Node!");
 });
 
-// 모든 연락처 가져오기
-app.get("/contacts", (req, res) => {
-    res.status(200).send("Contacts Page");
-});
+app.use(errorHandler);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/contacts", require("./routes/contactRoutes"));
 
-// 새 연락처 추가하기
-app.post("/contacts", (req, res) => {
-    res.status(201).send("Create Contacts");
-});
-
-// 연락처 상세 보기
-app.get("/contacts/:id", (req, res) => {
-    res.status(200).send(`View Contact for ID: ${req.params.id}`);
-});
-
-// 연락처 수정하기
-app.put("/contacts/:id", (req, res) => {
-    res.status(200).send(`Update Contact for ID: ${req.params.id}`);
-});
-
-// 연락처 삭제하기
-app.delete("/contacts/:id", (req, res) => {
-    res.status(200).send(`Delete Contact for ID: ${req.params.id}`);
-});
+// app.get("/test", (req, res) => {
+//   const error = new Error("테스트용 에러");
+//   error.status = 401;
+//   next(error);
+// });
 
 app.listen(port, () => {
     console.log(`${port}번 포트에서 서버 실행 중`);
